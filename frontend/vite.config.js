0490067686ed23a2563d9bb1f6759ab8e4116fe7
nittish03 <nittishbaboria123@gmail.com>
2025-01-18 12:57:03 +0530

    solving directory errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
-        outDir: 'public', // Ensure this matches what your system is expecting
+        outDir: 'dist', // Ensure this matches what your system is expecting
     },
 	plugins: [react()],
 	server: {
