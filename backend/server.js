01de72b83441224c757fb58c5ae568e253cdf2ed
nittish03 <nittishbaboria123@gmail.com>
2025-01-18 12:54:16 +0530

    solving directory errors

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,36 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/authRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import cors from 'cors'
-import connectDb from "./db/connectDb.js";
-import {app,server} from './socket/socket.js';
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
-app.use('/api/messages',messageRoutes);
-app.use('/api/users',userRoutes);
-app.use(cors());
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-	connectDb();
-	console.log(`Server Running on port ${PORT}`.bgCyan.white);
-  console.log();
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
