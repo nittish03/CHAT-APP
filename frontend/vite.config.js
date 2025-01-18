f6ef066702870fd3bae85f21cd14005c561ed49e
nittish03 <nittishbaboria123@gmail.com>
2025-01-18 19:43:57 +0530

    author

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
 		proxy: {
 			"/api": {
 				target: "https://chat-app-wrv0.onrender.com",
+				changeOrigin: true,
+				secure: false, // Bypass certificate validation
 			},
 		},
 	},
